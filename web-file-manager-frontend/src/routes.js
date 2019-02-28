60cf4d4ba6a981a8335bab04c4057b104d51756e
Nikita Korshunov <kewka96@gmail.com>
2019-02-28 22:32:06 +0500

    Initial commit

---
+++ web-file-manager-frontend/src/routes.js
@@ -0,0 +1,11 @@
+import { asyncComponent } from '@jaredpalmer/after';
+
+export default [
+  {
+    path: '/',
+    exact: true,
+    component: asyncComponent({
+      loader: () => import('~/pages/IndexPage')
+    })
+  }
+];
