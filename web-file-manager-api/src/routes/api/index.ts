7659a809ea848e5e119fee87710790dfeb000642
Nikita Korshunov <kewka96@gmail.com>
2019-03-04 14:49:31 +0500

    Add Host model

--- web-file-manager-api/src/routes/api/index.ts
+++ web-file-manager-api/src/routes/api/index.ts
@@ -4,6 +4,7 @@ import errorHandler from '@/utils/api/errorHandler';
 import directoryRouter from './directory';
 import fileRouter from './file';
 import drivesRouter from './drives';
+import hostRouter from './host';
 
 export default express.Router()
 // Api validations
@@ -12,5 +13,6 @@ export default express.Router()
 .use(directoryRouter)
 .use(fileRouter)
 .use(drivesRouter)
+.use(hostRouter)
 // Error handler
 .use(errorHandler);
